16a3e250a23aab8d75b524fc9cc9e1e3514de3cb
khansaabd <74952813+khansaabd@users.noreply.github.com>
2020-11-24 14:41:51 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$name="khansa abd almonam";$subject="php";$class="3it";$address="alkhartoum";$phone="0998688153";
+echo"name:".$name."<br>";
+echo"subject:".$subject."<br>";
+echo"class:".$class."<br>";
+echo"address:".$address."<br>";
+echo"phone:".$phone."<br>";
+?>    
+</body>
+</html>
